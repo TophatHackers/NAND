98770821956a5708650c405635dc3ee8261f539c
oskhen <oskar.henriksson2001@gmail.com>
2020-11-12 14:19:14 +0100

    Added AND sourcefile and started compiler

---
+++ AND.asm
@@ -0,0 +1,9 @@
+sys READ r0
+sys READ r1
+
+START r0 r1
+NAND r0 r1
+NAND rn rn
+END r2
+
+sys PRINT r2
---
+++ NAND/target/debug/NAND.d
@@ -0,0 +1,1 @@
+/home/oskar/Documents/Courses/DD1337-Programmering/NAND/NAND/target/debug/NAND: /home/oskar/Documents/Courses/DD1337-Programmering/NAND/NAND/src/main.rs
