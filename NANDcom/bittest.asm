f1b01d104d829a6df1ffa1a24f5af656f4fcadc8
oskhen <oskar.henriksson2001@gmail.com>
2020-11-15 14:45:32 +0100

    BIT instruction now properly increases PC

---
+++ NANDcom/bittest.asm
@@ -0,0 +1,6 @@
+sys READ r4 # read "1"
+sys READ r5 # read "1"
+
+BIT WRITE 5
+
+sys WRITE r4
--- NANDem/src/main.rs
+++ NANDem/src/main.rs
@@ -61,13 +61,13 @@ fn emulate_program(binary: Vec<String>) {
         
         let mut reached_end = false;
         unsafe {
-            //println!();
-            //println!("REGISTERS: {:?}", REGISTERS);
-            //println!("STACK: {:?}", STACK);
-            //println!("PROCESS: {:?}", PROCESS);
+            println!();
+            println!("REGISTERS: {:?}", REGISTERS);
+            println!("STACK: {:?}", STACK);
+            println!("PROCESS: {:?}", PROCESS);
             //println!("PROCESSSTACK: {:?}", PROCESSTACK);
             //println!("REGISTRYSTACK: {:?}", REGISTRYSTACK);
-            //println!("Current instruction: {}", instruction);
+            println!("Current instruction: {}", instruction);
         }
 
         let op = &instruction[0..2];
@@ -116,6 +116,7 @@ fn emulate_program(binary: Vec<String>) {
                 binr4[imm] = r5bit;
                 let u32r4: String = binr4.into_iter().collect();
                 REGISTERS[6] = u32::from_str_radix(&u32r4, 2).unwrap();
+                REGISTERS[0] += 1;
             }
         }
 
